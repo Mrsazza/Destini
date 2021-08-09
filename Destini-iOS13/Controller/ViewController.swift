91f3c04b4f37c3253f0c3f17271bc2541c074d38
Mrsazza <shahriarhossain147@gmail.com>
2021-08-09 09:21:06 +0600

    Choose your own adventure type game

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userChoice)
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = storyBrain.storyTitle()
+        choice1Button.setTitle(storyBrain.choice1Text(), for: .normal)
+        choice2Button.setTitle(storyBrain.choice2Text(), for: .normal)
+    }
+    
 }
 
